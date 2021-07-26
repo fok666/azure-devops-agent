5f0339449448f5bc8628f95a60f10bb8ad87242e
fok666 <44035492+fok666@users.noreply.github.com>
2021-07-26 16:19:25 -0300

    commit run.sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for R in `seq 1 $(lscpu -p=CPU | grep -v "^#" | wc -l)`; do
+  mkdir -p /mnt/agent${R}/w
+  sudo docker run --privileged -td --cpus="2" \
+  -e AZP_URL="$2" \
+  -e AZP_TOKEN="$3" \
+  -e AZP_POOL="$4" \
+  -e AZP_AGENT_NAME=sha-`hostname`-$R \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /mnt/agent${R}/w:/_work \
+  --name agent$R \
+  --restart always \
+  $1
+done
