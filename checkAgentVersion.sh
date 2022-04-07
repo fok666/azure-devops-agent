c9a6b0d2582c5f8851bd54bdcd06f79a61d06a4d
fok666 <44035492+fok666@users.noreply.github.com>
2022-04-07 14:22:12 -0300

    Check against latest tag

--- checkAgentVersion.sh
+++ checkAgentVersion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Check latest agent release version
-curl -sS "https://github.com/Microsoft/azure-pipelines-agent/releases.atom" | grep "<id>tag" | head -2 | tail -1 | grep -Eo "[0-9]+[.][0-9]+[.][0-9]+"
+curl -sSI "https://github.com/microsoft/azure-pipelines-agent/releases/latest" | grep "^location:" | grep -Eo "[0-9]+[.][0-9]+[.][0-9]+"
