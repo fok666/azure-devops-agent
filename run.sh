13c260897efa5bf1cb0328dbeee7e40415305c06
fok666 <44035492+fok666@users.noreply.github.com>
2022-09-02 15:54:04 -0300

    Limit CPU usage on lower-end environments
    When environment has 2 vCPUs or more, use 2 vCPUs per container.
    On systems with less than 2 vCPUs, use 1 vCPUs per container.

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-for R in `seq 1 $(lscpu -p=CPU | grep -v "^#" | wc -l)`; do
+CPU_COUNT=$(lscpu -p=CPU | grep -v "^#" | wc -l)
+MAX_CPU=$(($CPU_COUNT>1 ? 2 : 1))
+
+for R in `seq 1 $CPU_COUNT`; do
   sudo mkdir -p /mnt/agent${R}/w
-  sudo docker run --privileged -td --cpus="2" \
+  sudo docker run --privileged -td --cpus="${CPU_COUNT}" \
   -e AZP_URL="$2" \
   -e AZP_TOKEN="$3" \
   -e AZP_POOL="$4" \
