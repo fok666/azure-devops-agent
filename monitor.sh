86bbc38c125110110a9d86ccb7585cb971631d42
fok666 <44035492+fok666@users.noreply.github.com>
2022-09-05 10:01:31 -0300

    Shutdown event confirmation

--- monitor.sh
+++ monitor.sh
@@ -2,10 +2,20 @@
 
 # Monitors VMSS shutdown event, triggers agent shutdown
 
-curl -s 'http://169.254.169.254/metadata/scheduledevents?api-version=2020-07-01' -H 'Metadata: true' > /tmp/scheduledevents.json
+METADATA_ENDPOINT='http://169.254.169.254/metadata/scheduledevents?api-version=2020-07-01'
+
+curl -s $METADATA_ENDPOINT -H 'Metadata: true' > /tmp/scheduledevents.json
 grep -q "Terminate" /tmp/scheduledevents.json
+
 if [ $? -eq 0 ]; then
-  /opt/stop.sh  $1 $2 &
+
+  # Start agent shutdown
+  /opt/stop.sh
+
+  # Confirm event after agent shutdown:
+  EventId=`jq -r '.Events[] | .EventId' /tmp/scheduledevents.json`
+  curl -s -X POST $METADATA_ENDPOINT  -H 'Metadata: true' -d "{\"StartRequests\" : [{\"EventId\": \"${EventId}\"}]}"
+
 fi
 
 exit 0
